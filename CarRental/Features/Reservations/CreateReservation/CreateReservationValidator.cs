29bf0d4eb550371788cecce47e588448ea6dd659
Janek <janbarn00@gmail.com>
2025-04-01 22:23:49 +0200

    Added customer validation in reservation creation

--- CarRental/Features/Reservations/CreateReservation/CreateReservationValidator.cs
+++ CarRental/Features/Reservations/CreateReservation/CreateReservationValidator.cs
@@ -27,6 +27,23 @@ public class CreateReservationValidator : AbstractValidator<CreateReservationCom
             .Must(BeFullHour).WithMessage("Return date must be rounded to full hour.")
             .Must((cmd, returnDate) => (returnDate - cmd.PickupDate).TotalDays <= MaxRentalDays)
             .WithMessage($"Maximum rental period is {MaxRentalDays} days.");
+        
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email address.");
+
+        RuleFor(x => x.PhoneNumber)
+            .Matches(@"^\+[1-9]\d{1,14}$").WithMessage("Invalid phone number")
+            .When(x => x.PhoneNumber is not null);
+        
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required.")
+            .Length(2, 50).WithMessage("First name must be between 2 and 50 characters.");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required.")
+            .Length(2, 50).WithMessage("Last name must be between 2 and 50 characters.");
+            
     }
     private static bool BeFullHour(DateTime date)
     {
